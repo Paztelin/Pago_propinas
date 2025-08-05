e9173d3042310ca4f8ce234f862ee3860c49e949
Paztelin <monserratvinalaysuarez102.16@gmail.com>
2025-08-04 22:40:02 -0600

    pie de pagina dentro de calcu INICIO

--- funcionalidad.js
+++ funcionalidad.js
@@ -162,6 +162,7 @@ setTimeout(async () => {
                 if (restanteElemento) {
                     restanteElemento.textContent = `$${formatted}`;
                 }
+                await showAlert("Total de propinas registrado");
                 this.clear();
 
                 //paso3 - dividir propinas (opcional)
@@ -194,9 +195,37 @@ setTimeout(async () => {
                 divText.textContent = `$${propinaPorPersona.toFixed(2)} x persona`;
 
                 this.fixedMessage = `$${propinaPorPersona.toFixed(2)}`; //muestra monto propinas en display-calcu
+
+                const pie_pagina = document.getElementById("calculator")
+                const foot_calcu = document.createElement("div");
+
+                foot_calcu.innerHTML = `
+                    <p>Cantidad restante <strong> </strong></p<
+                `;
+
+                foot_calcu.style.borderRadius = "20px",
+                    foot_calcu.style.background = "#ffffff",
+                    foot_calcu.style.border = "2px solid #f89b8f",
+                    foot_calcu.style.margin = "15px 0",
+                    foot_calcu.style.padding = "0",
+                    foot_calcu.style.fontSize = "14px",
+                    foot_calcu.style.width = "100%",
+                    foot_calcu.style.height = "50px",
+                    foot_calcu.style.boxSizing = "border-box",
+                    foot_calcu.style.textAlign = "center",
+                    foot_calcu.style.color = "#e65c4f"
+
+                pie_pagina.appendChild(foot_calcu);
+
+                const strongElement = foot_calcu.querySelector("strong");
+                strongElement.textContent = this.totalPropina.toFixed;
+
+
+                console.log("Nuevo div añadido:", foot_calcu);
+
+
+                await showAlert("Ahora por favor elije el método de pago");
                 this.updateUI();
-                //this.dividingMode = false;
-                //this.clear();
             }
         }
 
@@ -246,5 +275,227 @@ setTimeout(async () => {
     btnEditar.addEventListener("click", () => {
         calculator.editInput();
     });
+
+
+    /////////////////////////////////////////////////////////////////////////
+
+
+    const registrosDerecha = document.getElementById("registrar-pagos");
+    const quitar_div = document.getElementById("registro-pagos");
+
+    let primerPagoSeleccionado = false; //sbaer si ya se borró el div que ya está
+
+    const pagos = [
+        {
+            id: "pago1",
+            svg: `<svg class="icono-bill" width="30" height="30" viewBox="0 0 48 48" fill="none">
+                <path d="M3 20 v21 h35" fill="none" stroke="#333" stroke-width="3.5" stroke-linejoin="bevel" />
+                <rect x="10" y="14" width="32" height="20" rx="2" fill="none" stroke="#333" stroke-width="3.5" />
+                <circle cx="26" cy="24" r="4" fill="#333" stroke="none" />
+                <circle cx="13" cy="17" r="3.5" fill="#333" />
+                <circle cx="39" cy="17" r="3.5" fill="#333" />
+                <circle cx="13" cy="31" r="3.5" fill="#333" />
+                <circle cx="39" cy="31" r="3.5" fill="#333" />
+            </svg>`,
+            texto: "Efectivo",
+            svg2: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                stroke="#e65c4f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/>
+                </svg>`
+        },
+        {
+            id: "pago2",
+            svg: `
+            <svg class="icono-svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#333"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <rect x="2" y="5" width="20" height="14" rx="2" ry="2"></rect>
+                <line x1="3" y1="10" x2="21" y2="10" stroke-width="4"></line>
+            </svg>`,
+            texto: "BBVA 1234",
+            svg2: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                stroke="#e65c4f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/>
+                </svg>`
+        },
+        {
+            id: "pago3",
+            svg: `
+            <svg class="icono-svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#333"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <rect x="2" y="5" width="20" height="14" rx="2" ry="2"></rect>
+                <line x1="3" y1="10" x2="21" y2="10" stroke-width="4"></line>
+            </svg>`,
+            texto: "Santander 1234",
+            svg2: `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                stroke="#e65c4f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/>
+                </svg>`
+        }
+    ];
+
+    // Crear div dinámico para los 3tipos de pago
+    function crearDivPago(pago) {
+        const div = document.createElement("div");
+        div.classList.add("pago-seleccionado");
+
+        div.innerHTML =
+            `${pago.svg}
+        <p>${pago.texto}</p>
+        ${pago.svg2}`;
+
+        Object.assign(div.style, {
+            borderRadius: "15px",
+            padding: "15px 25px",
+            width: "90%",
+            height: "25px",
+            textAlign: "left",
+            boxShadow: "3px 3px 5px lightgray, -3px 0 5px lightgray",
+            fontSize: "15px",
+            color: "#333",
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            marginBottom: "10px",
+        });
+
+        return div;
+    }
+
+    // Eventos de click para cada pago
+    pagos.forEach(pago => {
+        const boton = document.getElementById(pago.id);
+        if (!boton) return;
+
+        boton.addEventListener("click", () => {
+            // Oculta el div inicial solo la primera vez
+            if (!primerPagoSeleccionado) {
+                quitar_div.style.display = "none";
+                primerPagoSeleccionado = true;
+            }
+
+            const nuevoDiv = crearDivPago(pago);
+            registrosDerecha.appendChild(nuevoDiv);
+        });
+    });
+
+
+    /*
+
+    pago1.addEventListener("click", () => {
+        // Oculta el div que tiene 
+        quitar_div.style.display = "none";
+
+        // Limpia el contenedor
+        registrosDerecha.innerHTML = "";
+
+        // Crea div nuevo
+        const pago1_seleccionado = document.createElement("div");
+        pago1_seleccionado.innerHTML =
+            `< svg class="icono-bill" width = "30" height = "30" viewBox = "0 0 48 48" fill = "none" >
+                            <path d="M3 20 v21 h35" fill="none" stroke="#333" stroke-width="3.5"
+                                stroke-linejoin="bevel" />
+                            <!--Billete delantero-- >
+                            <rect x="10" y="14" width="32" height="20" rx="2" fill="none" stroke="#333"
+                                stroke-width="3.5" />
+                            <!--círculo central-- >
+                            <circle cx="26" cy="24" r="4" fill="#333" stroke="none" />
+                            <!--círculos en las esquinas internas-- >
+                            <circle cx="13" cy="17" r="3.5" fill="#333" /> <!--sup izq-- >
+                            <circle cx="39" cy="17" r="3.5" fill="#333" /> <!--sup der-- >
+                            <circle cx="13" cy="31" r="3.5" fill="#333" /> <!--inf izq-- >
+                            <circle cx="39" cy="31" r="3.5" fill="#333" /> <!--inf der-- >
+            </svg >
+                <p>Efectivo</p>`;
+
+        //diseño del div nuevo -mismo que css
+        Object.assign(pago1_seleccionado.style, {
+            borderRadius: "15px",
+            padding: "15px 25px",
+            width: "90%",
+            height: "25px",
+            textAlign: "left",
+            boxShadow: "3px 3px 5px lightgray, -3px 0 5px lightgray",
+            fontSize: "15px",
+            color: "#333",
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            gap: "10px",
+        });
+
+        // inserta el nuevo div
+        registrosDerecha.appendChild(pago1_seleccionado);
+
+        console.log("Nuevo div añadido:", pago1_seleccionado);
+    });
+
+    pago2.addEventListener("click", () => {
+        // Crea div nuevo
+        const pago2_seleccionado = document.createElement("div");
+        pago2_seleccionado.innerHTML =
+            `< svg class="icono-svg" width = "30" height = "30" viewBox = "0 0 24 24" fill = "none" stroke = "#333"
+            stroke - width="2" stroke - linecap="round" stroke - linejoin="round" >
+                            <rect x="2" y="5" width="20" height="14" rx="2" ry="2"></rect>
+                            <line x1="3" y1="10" x2="21" y2="10" stroke-width="4"></line>
+                        </svg >
+                <p>BBVA 123 ${fixedMessage}</p>`;
+
+        //diseño del div nuevo -mismo que css
+        Object.assign(pago2_seleccionado.style, {
+            borderRadius: "15px",
+            padding: "15px 25px",
+            width: "90%",
+            height: "25px",
+            textAlign: "left",
+            boxShadow: "3px 3px 5px lightgray, -3px 0 5px lightgray",
+            fontSize: "15px",
+            color: "#333",
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            gap: "10px",
+        });
+
+        // inserta el nuevo div
+        registrosDerecha.appendChild(pago2_seleccionado);
+
+        console.log("Nuevo div añadido:", pago2_seleccionado);
+    });
+
+    pago3.addEventListener("click", () => {
+        // Crea div nuevo
+        const pago3_seleccionado = document.createElement("div");
+        pago3_seleccionado.innerHTML =
+            `< svg class="icono-svg" width = "30" height = "30" viewBox = "0 0 24 24" fill = "none" stroke = "#333"
+            stroke - width="2" stroke - linecap="round" stroke - linejoin="round" >
+                            <rect x="2" y="5" width="20" height="14" rx="2" ry="2"></rect>
+                            <line x1="3" y1="10" x2="21" y2="10" stroke-width="4"></line>
+                        </svg >
+                <p>Santander 123 </p>`;
+
+        //diseño del div nuevo -mismo que css
+        Object.assign(pago3_seleccionado.style, {
+            borderRadius: "15px",
+            padding: "15px 25px",
+            width: "90%",
+            height: "25px",
+            textAlign: "left",
+            boxShadow: "3px 3px 5px lightgray, -3px 0 5px lightgray",
+            fontSize: "15px",
+            color: "#333",
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            gap: "10px",
+        });
+
+        // inserta el nuevo div
+        registrosDerecha.appendChild(pago3_seleccionado);
+
+        console.log("Nuevo div añadido:", pago3_seleccionado);
+    });*/
 
 }, 1500);
